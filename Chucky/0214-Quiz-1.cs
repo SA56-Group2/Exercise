5f9ffa309d96ea21ae3613543d3e35fd6340518a
Chucky <124947089+ChuckyJI@users.noreply.github.com>
2023-02-14 14:18:57 +0800

    Add files via upload

---
+++ Chucky/0214-Quiz-1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Quiz_0214
+{
+    internal class Program
+    {
+        static int[] minIncomeArray = new int[]
+        { 20000, 30000, 40000, 80000,
+            120000, 160000, 200000, 320000 };
+        static double[] taxRateArray = new double[]
+        { 0.02, 0.035, 0.07, 0.115,
+            0.15, 0.17, 0.18, 0.20 };
+        static int[] basePayableAmountArray = new int[]
+        { 0, 200, 550, 3350,
+            7950, 13950, 20750, 42350 };
+        static void Main(string[] args)
+        {
+            int annualIncome = AskForIncome();
+            int taxBracket = GetTaxBracket(annualIncome);
+            double taxPayable =
+                CalculateIncomeTax(annualIncome, taxBracket);
+            PrintResult(annualIncome, taxPayable);
+        }
+
+        static int AskForIncome()
+        {
+            Console.Write("Please enter your annual taxable income: ");
+            int annualIncome = int.Parse(Console.ReadLine());
+            return annualIncome;
+        }
+        static int GetTaxBracket(int annualIncome)
+        {
+            int taxBracket = 0;
+            if ((annualIncome >= 0) && (annualIncome < minIncomeArray[1])) taxBracket = -1;
+            else if ((annualIncome >= minIncomeArray[0]) && (annualIncome < minIncomeArray[1]))
+                taxBracket = 1;
+            else if ((annualIncome >= minIncomeArray[1]) && (annualIncome < minIncomeArray[2]))
+                taxBracket = 2;
+            else if ((annualIncome >= minIncomeArray[2]) && (annualIncome < minIncomeArray[3]))
+                taxBracket = 3;
+            else if ((annualIncome >= minIncomeArray[3]) && (annualIncome < minIncomeArray[4]))
+                taxBracket = 4;
+            else if ((annualIncome >= minIncomeArray[4]) && (annualIncome < minIncomeArray[5]))
+                taxBracket = 5;
+            else if ((annualIncome >= minIncomeArray[5]) && (annualIncome < minIncomeArray[6]))
+                taxBracket = 6;
+            else if ((annualIncome >= minIncomeArray[6]) && (annualIncome < minIncomeArray[7]))
+                taxBracket = 7;
+            else if (annualIncome >= minIncomeArray[7]) taxBracket = 8;
+            return taxBracket;
+        }
+
+        static double CalculateIncomeTax(int anuualIncome, double taxBracket)
+        {
+            double taxpayable;
+            switch (taxBracket)
+            {
+                case -1:
+                {
+                    taxpayable = 0;
+                    return taxpayable;
+                }
+                case 1:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[0]) * taxRateArray[0] + basePayableAmountArray[0];
+                    return taxpayable;
+                }
+                case 2:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[1]) * taxRateArray[1] + basePayableAmountArray[1];
+                    return taxpayable;
+                }
+                case 3:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[2]) * taxRateArray[2] + basePayableAmountArray[2];
+                    return taxpayable;
+                }
+                case 4:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[3]) * taxRateArray[3] + basePayableAmountArray[3];
+                    return taxpayable;
+                }
+                case 5:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[4]) * taxRateArray[4] + basePayableAmountArray[4];
+                    return taxpayable;
+                }
+                case 6:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[5]) * taxRateArray[5] + basePayableAmountArray[5];
+                    return taxpayable;
+                }
+                case 7:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[6]) * taxRateArray[6] + basePayableAmountArray[6];
+                    return taxpayable;
+                }
+                case 8:
+                {
+                    taxpayable = (anuualIncome - minIncomeArray[7]) * taxRateArray[7] + basePayableAmountArray[7];
+                    return taxpayable;
+                }
+            }
+            return 0;
+        }
+
+        static void PrintResult(int annualIncome,double taxPayable)
+        {
+            Console.WriteLine("For taxable annual income of {0:C}, the tax payable amount is {1:C}",annualIncome,taxPayable);
+        }
+    }
+}
---
+++ Chucky/0214-Quiz-2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Quiz_0214_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string upperPlainText = ReturnUpperInputSentence(); 
+            string encryptedText = EncryptSentence(upperPlainText);
+            PrintEncryptedSentence(encryptedText);
+            string decryptedText = DecrpytSentence(encryptedText); 
+            PrintDecryptedSentence(decryptedText);
+            Console.WriteLine("\nType any key to exit.");
+            Console.ReadLine();
+        }
+
+        static string ReturnUpperInputSentence()
+        {
+            Console.Write("Please enter the sentence: ");
+            string str = Console.ReadLine();
+            string numberUpper = str.ToUpper();
+            return numberUpper;
+        }
+
+        static string EncryptSentence(string upperPlainText)
+        {
+            string letterlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZABC";
+            string newUp = null;
+            string med = upperPlainText;
+            for (int i = 0; i < upperPlainText.Length; i++)
+            {
+                
+                for (int j = 0; j < letterlist.Length; j++)
+                {
+                    string a = med.Substring(i, 1);
+                    string b = letterlist.Substring(j, 1);
+                    string c = letterlist.Substring(j+3, 1);
+                    if (a == b)
+                    {
+                        newUp = med.Replace(a,c);
+                        med = newUp;
+                        break;
+                    }
+                }
+            }
+            return med;
+        }
+
+        static void PrintEncryptedSentence(string encryptedText)
+        {
+            Console.WriteLine("The encrypted sentence is :{0}",encryptedText);
+        }
+
+        static string DecrpytSentence(string encryptedText)
+        {
+            string letterlist = "CBAZYXWVUTSRQPONMLKJIHGFEDCBA";
+            string newUp = null;
+            string med = encryptedText;
+            for (int i = 0; i < encryptedText.Length; i++)
+            {
+                for (int j = 0; j < letterlist.Length; j++)
+                {
+                    string a = med.Substring(i, 1);
+                    string b = letterlist.Substring(j, 1);
+                    string c = letterlist.Substring(j+3, 1);
+                    if (a == b)
+                    {
+                        newUp = med.Replace(a,c);
+                        med = newUp;
+                        break;
+                    }
+                }
+            }
+            return med;
+        }
+
+        static void PrintDecryptedSentence(string decryptedText)
+        {
+            Console.WriteLine("The decrpyted sentence is :{0}",decryptedText);
+
+        }
+    }
+}
