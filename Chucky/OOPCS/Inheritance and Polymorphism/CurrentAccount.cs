d49fd3b3f2ae3d403b495e41ad7db4660f673897
Chucky <124947089+ChuckyJI@users.noreply.github.com>
2023-02-17 21:55:18 +0800

    Add files via upload

---
+++ Chucky/OOPCS/Inheritance and Polymorphism/CurrentAccount.cs
@@ -0,0 +1,47 @@
+namespace Class
+{
+    public class CurrentAccount : Account
+    {
+        private double interestRate = 0.025;
+
+        public double GetInterestRate
+        {
+            get { return interestRate;}
+            set { interestRate = value; }
+        }
+
+        public CurrentAccount() : base()
+        {
+            interestRate = 0.0025;
+        }
+
+        public CurrentAccount(string acctnumber, string acctholdId, double balance1) : base(
+            acctnumber,acctholdId, balance1)
+        {
+        }
+
+        public double CalculateInterest()
+        {
+            double interest = interestRate * GetBalance;
+            return interest;
+        }
+
+        public void CreditInterest()
+        {
+            double interest = CalculateInterest();
+            base.Deposit(interest);
+        }
+        
+        public bool Withdraw(double amt)
+        {
+            GetBalance -= amt;
+            bool result = GetBalance < 0 ? false : true;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Chucky/OOPCS/Inheritance and Polymorphism/OverdraftAccont.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace Class
+{
+    public class OverdraftAccount : Account
+    {
+        private double interestRateP = 0.025;
+        private double interestRateN = 0.06;
+
+        public double GetInterestRateP
+        {
+            get { return interestRateP;}
+            set { interestRateP = value; }
+        }
+
+        public double GetInterestRateN
+        {
+            get { return interestRateN;}
+            set { interestRateN = value;}
+        }
+
+        public OverdraftAccount() : base()
+        {
+            interestRateP = 0.0025;
+            interestRateN = 0.06;
+        }
+
+        public OverdraftAccount(string acctnumber, string acctholdId, double balance1) : base(
+            acctnumber,acctholdId, balance1)
+        {
+        }
+
+        public double CalculateInterest()
+        {
+            double interestRate = GetBalance >= 0 ? interestRateP : interestRateN;
+            double interest = interestRate * GetBalance;
+            return interest;
+        }
+
+        public void CreditInterest()
+        {
+            double interest = CalculateInterest();
+            base.Deposit(interest);
+        }
+        
+        public void Withdraw(double amt)
+        {
+            GetBalance -= amt;
+            Console.WriteLine("Operation Succees!");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
